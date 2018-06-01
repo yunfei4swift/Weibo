01f90918a06f8b01bba09d2502a706259fb76fbd
Austin <yunfei4cs@gmail.com>
2018-06-01 13:43:42 +0000

    update symbols

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hello Laravel! - by Austin</div>
+                <div class="title">Hello Laravel!!! - by Austin</div>
             </div>
         </div>
     </body>
